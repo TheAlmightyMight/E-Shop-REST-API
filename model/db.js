64510738eb39304870e7b0467b2d33ec965b0989
Klimenty Karavaev <klimentykk@gmail.com>
2022-11-04 00:09:50 +0400

    Create a class for for db interaction

--- model/db.js
+++ model/db.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
 
+class DataBase {
+  connect(options = {}) {
+    mongoose.connect(process.env.MONGODB_CONNECTION_STRING, options, (err) => {
+      try {
+        if (err) {
+          throw err;
+        } else {
+          console.log("Connected");
+        }
+      } catch (err) {
+        console.error("Having trouble connecting to MongoDB", err.message);
+      }
+    });
+  }
+}
