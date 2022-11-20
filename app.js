76eb32a24f50f0ff042d80917802a6f864e08a05
Klimenty Karavaev <klimentykk@gmail.com>
2022-11-20 15:42:28 +0400

    Download types for mongoose, edit app

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-// const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const compression = require("compression");
-const mongoose = require("mongoose");
+const db = require("./model/db");
 
 require("dotenv").config();
 
 const productsRouter = require("./routes/products");
 const signUpRouter = require("./routes/signUp");
 const cartRouter = require("./routes/cart");
+const loginRouter = require("./routes/login");
+const logOutRouter = require("./routes/logout");
 
 const app = express();
-
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {}, (err) => {
-  if (err) {
-    console.error("Having trouble connecting to MongoDB", err.message);
-    throw err;
-  } else {
-    console.log("Connected");
-  }
-});
+db.connect();
 
 app.use(logger(":method :url :status :remote-addr :user-agent"));
 app.use(express.json());
-app.use(compression({ threshold: 1024 }));
 app.use(cookieParser());
+app.use(compression({ threshold: 1024 }));
+app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({ origin: process.env.MODE === "dev" ? "*" : process.env.FRONTEND_URL })
 );
+
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/login", loginRouter);
+app.use("/logout", logOutRouter);
 app.use("/products", productsRouter);
 app.use("/signUp", signUpRouter);
-app.use("/users", cartRouter);
+app.use("/cart", cartRouter);
+
+app.get("/", async (req, res) => {
+  try {
+    res.send("oops");
+    throw new Error("Foo");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`App listening on port ${process.env.PORT}`);
