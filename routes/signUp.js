3c8ab445983eae90b9067237c9951014d1e6cca1
Klimenty Karavaev <klimentykk@gmail.com>
2022-11-20 15:43:23 +0400

    Edit signUp route

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const userSchema = require("../model/schemas/userSchema");
+const loginUser = require("../middleware/userLogIn");
 
 const router = express.Router();
 
 // SignUp logic
-router.post("/", async (req, res, next) => {
+router.post("/", async (req, res) => {
   const { email, password, name, surname, role } = req.body;
   try {
     const user = await userSchema.signUp(email, password, name, surname, role);
-    res.status(201).json(user);
+    if (user) {
+      loginUser(user, res);
+    } else {
+      throw new Error("Could not create user");
+    }
   } catch (err) {
     console.error(err.message);
-    res.json(err.message);
+    res.json({ msg: err.message });
   }
 });
 
